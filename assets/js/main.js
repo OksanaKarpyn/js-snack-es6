0b839c22b914f6131a2ed0ec58b3e3af2c15cb25
OksanaKarpyn <93349703+OksanaKarpyn@users.noreply.github.com>
2023-04-14 18:53:55 +0200

    add secondo esercizio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,11 @@
-let arrayOggetti = [{
-        nome: 'prima bici',
-        peso: 10
-    },
+let arrayOggetti = [
     {
+        nome: ' prima bici',
+        peso: 10
+    }, {
         nome: 'seconda bici ',
         peso: 15
-    },
-    {
+    }, {
         nome: 'terza bici ',
         peso: 20
     }
@@ -14,39 +13,39 @@ let arrayOggetti = [{
 let item = document.querySelector('.item');
 let pesoMinimo = arrayOggetti[0].peso;
 for (let i = 0; i < arrayOggetti.length; i++) {
-    let {
-        peso,
-        nome
-    } = arrayOggetti[i];
+    let {peso, nome} = arrayOggetti[i];
     // console.log(peso,nome)
     if (pesoMinimo == arrayOggetti[i].peso) {
-    item.innerHTML += `<div class="">${peso}${nome}</div>`
+        item.innerHTML += `<div class="">${peso}${nome}</div>`
         // console.log(pesoMinimo)
     }
 }
 
-
-
 // -----------2esercizio----------
-// let random = Math.floor(Math.random() * 10) + 1;
-// let arraySquadre = [{
-//         nome: 'prima squadra',
-//         punti: random,
-//         perse: random
-//     },
-//     {
-//         nome: 'seconda squadra',
-//         punti: random,
-//         perse: random
+let random = Math.floor(Math.random() * 10) + 1;
+let arraySquadre = [
+    {
+        nome: 'prima squadra',
+        punti: random,
+        perdite: random
+    }, {
+        nome: 'seconda squadra',
+        punti: random,
+        perdite: random
 
-//     },
-//     {
-//         nome: 'terza squadra',
-//         punti: random,
-//         perse: random
+    }, {
+        nome: 'terza squadra',
+        punti: random,
+        perdite: random
 
-//     }
-// ]
-// for (let i = 0; i < arraySquadre.length; i++) {
+    }
+]
+let arrNew = []
+for (let i = 0; i < arraySquadre.length; i++) {
+    let {punti, perdite} = arraySquadre[i]
+    
+    arrNew.push(punti, perdite)
+    console.log(arrNew)
 
-// }
+    // console.log(nome, punti, perdite)
+}
