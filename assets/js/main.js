0cbfc3fc4c1a83f116920ac82e3f023d4f69e33b
OksanaKarpyn <93349703+OksanaKarpyn@users.noreply.github.com>
2023-04-17 19:11:02 +0200

    add esercizio 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+// ---------esercizio 1----------------
+
 let arrayOggetti = [{
     nome: 'prima bici',
     peso: 30
@@ -8,32 +10,30 @@ let arrayOggetti = [{
     nome: 'terza bici ',
     peso: 20
 }]
-let item =document.querySelector('.item')
+let item = document.querySelector('.item')
 let minimo = arrayOggetti[0];
 arrayOggetti.forEach((element) => {
- 
-    if (element.peso < minimo.peso){
-        minimo=element;
+
+    if (element.peso < minimo.peso) {
+        minimo = element;
     }
-  
-
-
 })
-const {nome,peso}=minimo;
-console.log(nome,peso)
-item.innerHTML+=` la ${nome} ha il peso ${peso}`
+const {
+    nome,
+    peso
+} = minimo;
+console.log(nome, peso)
+item.innerHTML += ` la ${nome} ha il peso ${peso}`
+// console.log(minimo.peso,minimo.nome)
 
-console.log(minimo.peso,minimo.nome)
 
 // const sorted = arrayOggetti.sort((a, b) => {
 //     if (a.peso > b.peso) {
 //         return 1;
-
 //     }
 //     if(a.peso < b.peso){
 //         return -1;
 //     }
-
 //     return 0;
 // })
 // let { nome,peso}=sorted[0]
@@ -56,16 +56,22 @@ let arraySquadre = [{
 
 }, {
     nome: 'terza squadra',
-    punti:0,
+    punti: 0,
     perdite: 0
 
 }]
-const squadrePerdite =[];
-arraySquadre.forEach((element,i)=>{
-    element.punti= Math.floor(Math.random() * 10) + 1;
-    element.perdite= Math.floor(Math.random() * 5) + 1;
-    let {nome,perdite}=element
-    squadrePerdite.push({nome,perdite})
+const squadrePerdite = [];
+arraySquadre.forEach((element, i) => {
+    element.punti = Math.floor(Math.random() * 10) + 1;
+    element.perdite = Math.floor(Math.random() * 5) + 1;
+    let {
+        nome,
+        perdite
+    } = element
+    squadrePerdite.push({
+        nome,
+        perdite
+    })
 });
 
 console.log(squadrePerdite)
@@ -80,17 +86,14 @@ console.log(squadrePerdite)
 
 // console.log(squadrePerdite)
 
-
-
-
-
-
-
-
-
-
-
 // ----------esercizio 3-------
+/* Snack 3
+Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+l'array egnerato da parte vostra dovrà essere di ad esempio nomi o cose: ['alberto', 'michele','angelo','simone']
+se come numeri ad esemio ho 2 e 4 devo inserire in un array nuovo l'elemento in posizione 3
+La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+Usiamo i nuovi metodi degli array foreach o filter. */
+
 let arrCitta = [
     'milano',
     'roma',
@@ -99,4 +102,30 @@ let arrCitta = [
     'firenze',
     'trieste'
 ];
+let newCitta = []
+let min = parseInt(prompt(`inserisi il numero tra 0 e ${arrCitta.length-1}`));
+let max = parseInt(prompt(`inserisi il  numerotra 0 e  ${arrCitta.length-1}`));
+// arrCitta.forEach((element, i) => {
+//     if (min < i && max > i) {
+//         newCitta.push(element)
+//     }
+// })
 
+// let newAraayCitta = arrCitta.filter((element, i) => {
+//     if (min < i && max > i) {
+//         return element
+//     }
+// })
+
+function myfunction(parArray, valoreMin, valoreMax) { //parArray=arrCitta,valoreMax= max,valoreMin=min
+    let newarrCitta = parArray.filter((element, i) => {
+        if (valoreMin < i && valoreMax > i) {
+            return element
+        }
+
+    })
+  return newarrCitta
+}
+
+myfunction(arrCitta, min, max);
+console.log(myfunction(arrCitta, min, max) );
